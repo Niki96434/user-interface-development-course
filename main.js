620fd7bf3fd83cf0e2574bf04e0b291d3b839322
nikki <cozercaniegirl143@mail.ru>
2026-09-21 15:50:39 +0300

    feat: 8/8 fourth task

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 import firstTask from './3.09.26-7.09.26/practice-1-task.js';
 import secondTask from './3.09.26-7.09.26/practice-2-task.js';
 import thirdTask from './3.09.26-7.09.26/practice-3-task.js';
+import fourthTask from './3.09.26-7.09.26/practice-4-task.js';
 
 firstTask();
 secondTask();
 thirdTask();
+fourthTask();
